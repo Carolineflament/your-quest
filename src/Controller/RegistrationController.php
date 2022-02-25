a08e36bfa5eeb8e97a7a612b8449fa0f601a94f5
Sébastien <sgeraudie@gmail.com>
2022-02-25 17:45:49 +0100

    add register form + modify formtype

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Repository\RoleRepository;
 use App\Security\LoginFormAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,30 +20,38 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/inscription", name="app_register")
      */
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, LoginFormAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, LoginFormAuthenticator $authenticator, EntityManagerInterface $entityManager, RoleRepository $roleRepos): Response
     {
         $user = new User();
+        $user->setCreatedAt(new \DateTimeImmutable('now'));
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
+        
 
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
             $user->setPassword(
             $userPasswordHasher->hashPassword(
                     $user,
-                    $form->get('plainPassword')->getData()
+                    $form->get('password')->getData()
                 )
             );
+            //TODO attention ici on met le role en dure
+            $role = $roleRepos->find(1);
+            $user->setRole($role);
+            $user->setStatus(true);
+            
 
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
-
-            return $userAuthenticator->authenticateUser(
-                $user,
-                $authenticator,
-                $request
+            $this->addFlash(
+                'notice-success',
+                'L\'acteur  a été ajouté !'
             );
+            //TODO mettre la route du back si c'est un organisateur et la page d'acceuil si c'est un user
+            return $this->redirectToRoute('front_main', [], Response::HTTP_SEE_OTHER);
+            
         }
 
         return $this->render('registration/register.html.twig', [
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,45 +5,143 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'label' => 'E-mail : ',
+                'label_attr' => ['class' => 'visually-hidden'],
+                'attr' => ['placeholder' => 'Email']
             ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('password', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'label' => 'Votre mot de passe : ',
+                'label_attr' => ['class' => 'visually-hidden'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez renseigner un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Le mot de passe doit faire minimum {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
+                'attr' => ['autocomplete' => 'new-password', 'placeholder' => 'Mot de passe'],
+            ])
+            ->add('username', TextType::class, [
+                'required' => true,
+                'label' => 'Votre pseudo : ',
+                'label_attr' => ['class' => 'visually-hidden'],
+                'attr' => ['placeholder' => 'Pseudo']
+            ])
+            ->add('lastname', TextType::class, [
+                'required' => true,
+                'label' => 'Votre nom : ',
+                'label_attr' => ['class' => 'visually-hidden'],
+                'attr' => ['placeholder' => 'Nom']
+            ])
+            ->add('firstname', TextType::class, [
+                'required' => true,
+                'label' => 'Votre prénom : ',
+                'label_attr' => ['class' => 'visually-hidden'],
+                'attr' => ['placeholder' => 'Prénom']
+            ])
+            ->add('address', TextareaType::class, [
+                'label' => 'Votre adresse :',
+                'label_attr' => ['class' => 'visually-hidden'],
+                'attr' => ['placeholder' => 'Adresse']
+            ])
+            ->add('postal_code', NumberType::class, [
+                'label' => 'Code postal :',
+                'label_attr' => ['class' => 'visually-hidden'],
+                'html5' => true,
+                'attr' => ['placeholder' => 'Code postal']
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Votre ville :',
+                'label_attr' => ['class' => 'visually-hidden'],
+                'attr' => ['placeholder' => 'Ville']
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter les conditions d\'utilisation.',
+                    ]),
+                ],
             ])
         ;
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
+            // On récupère le form depuis l'event (pour travailler avec)
+            $form = $event->getForm();
+            // On récupère le user mappé sur le form depuis l'event
+            $user = $event->getData();
+
+            // On conditionne le champ "password"
+            // Si user existant, il a id non null
+            if ($user->getId() !== null) {
+                // Edit
+                $form->add('password', PasswordType::class, [
+                    // Pour le form d'édition, on n'associe pas le password à l'entité
+                    // @link https://symfony.com/doc/current/reference/forms/types/form.html#mapped
+                    'mapped' => false,
+                    'attr' => [
+                        'placeholder' => 'Laissez vide si inchangé'
+                    ]
+                ]);
+            } else {
+                // New
+                $form->add('password', PasswordType::class, [
+                    // En cas d'erreur du type
+                    // Expected argument of type "string", "null" given at property path "password".
+                    // (notamment à l'edit en cas de passage d'une valeur existante à vide)
+                    'empty_data' => '',
+                    'mapped' => false,
+                    'label' => 'Votre mot de passe : ',
+                    'label_attr' => ['class' => 'visually-hidden'],
+                    
+                    'attr' => ['autocomplete' => 'new-password', 'placeholder' => 'Mot de passe'],
+                    // On déplace les contraintes de l'entité vers le form d'ajout
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez renseigner un mot de passe',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Le mot de passe doit faire minimum {{ limit }} caractères',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                        /*new Regex(
+                            "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/",
+                            "Le mot de passe doit contenir au minimum 8 caractères, une majuscule, un chiffre et un caractère spécial"
+                        ),*/
+                    ],
+                ]);
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
