57ec59178547775631ed3782a356a0412655f8c3
Carolineflament <caroline.flament1@gmail.com>
2022-02-28 15:32:38 +0100

    GameType filter

--- src/Form/GameType.php
+++ src/Form/GameType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Game;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,17 +17,39 @@ class GameType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('slug')
-            ->add('address')
-            ->add('postalCode')
-            ->add('city')
-            ->add('image')
-            ->add('summary')
-            ->add('status')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('user')
+            ->add('title', TextType::class, [
+                'label' => 'Nom du jeu :',
+                'attr' => ['placeholder' => 'Dans la peau de Louis Pasteur']
+            ])
+            ->add('address', TextareaType::class, [
+                'label' => 'Adresse du jeu :',
+                'attr' => ['placeholder' => '11 rue des camélias']
+            ])
+            ->add('postalCode', NumberType::class, [
+                'label' => 'Code postal :',
+                'html5' => true,
+                'attr' => ['placeholder' => '59000']
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville :',
+                'attr' => ['placeholder' => 'Lille']
+            ])
+            ->add('image', FileType::class, [
+                'required' => false,
+            ])
+            ->add('summary', TextType::class, [
+                'required' => false,
+                'label' => 'Description du jeu :',
+            ])
+            ->add('status',  ChoiceType::class,[
+                'label' => 'Etat du jeu',
+                'choices' => [
+                    'Actif' => 1,
+                    'Inactif' => 2,
+                ],
+                // 'placeholder' => 'Statut du jeu',
+                // 'label' => false,
+            ])
         ;
     }
 
