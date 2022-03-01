1ddf556cc961bae589bbabb8a42842fdcb912ba8
Damien-Oliveira <oliveiradamien123@gmail.com>
2022-03-01 17:38:54 +0100

    Update form checkpointType

--- src/Form/CheckpointType.php
+++ src/Form/CheckpointType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Checkpoint;
 use App\Entity\Game;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,14 +16,23 @@ class CheckpointType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('successMessage')
-            ->add('orderCheckpoint')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('isTrashed')
+            ->add('title', TextType::class, [
+                'label' => 'Nom de votre checkpoint :',
+            ])
+            ->add('successMessage', TextType::class, [
+                'label' => 'Message de félicitation:',
+            ])
+            ->add('orderCheckpoint', TextType::class, [
+                'label' => 'Ordre du checkpoint :',
+            ])
+            ->add('createdAt', DateTimeType::class, [
+                'widget' => 'single_text',
+                'label' => 'Ce checkpoint est créé le :',
+                'input' => 'datetime_immutable',
+            ])
             ->add('game', EntityType::class, [
                 'class' => Game::class,
+                'label' => 'Nom du jeu',
                 'choice_label' => 'slug',
             ])
         ;
