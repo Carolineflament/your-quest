da883cd9d76ae47e2af885957a808fc3272eca8c
Sébastien <sgeraudie@gmail.com>
2022-03-03 12:09:38 +0100

    add dynimic carousel on front page

--- src/Controller/Front/MainController.php
+++ src/Controller/Front/MainController.php
@@ -2,19 +2,23 @@
 
 namespace App\Controller\Front;
 
+use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
-{
+{    
     /**
-     * @Route("/", name="front_main")
+     * @Route("/", name="front_main", methods={"GET"})
      */
-    public function index(): Response
-    {
+    public function index(GameRepository $gameRepository, Request $request): Response
+    {    
+        $games = $gameRepository->findBy(['isTrashed' => 0, 'status' => 1], ['createdAt' => 'ASC']);
+
         return $this->render('front/main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'games' => $games,
         ]);
     }
 }
