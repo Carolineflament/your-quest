7b76500985dc264fe0802875bb5e546120ff3c4f
Sébastien <sgeraudie@gmail.com>
2022-03-07 10:46:05 +0100

    translate reset password subject

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -176,7 +176,7 @@ class ResetPasswordController extends AbstractController
         $email = (new TemplatedEmail())
             ->from(new Address('sgeraudie@gmail.com', 'Your Quest'))
             ->to($user->getEmail())
-            ->subject('Your password reset request')
+            ->subject('Votre demande de réinitialisation de mot de passe')
             ->htmlTemplate('reset_password/email.html.twig')
             ->context([
                 'resetToken' => $resetToken,
