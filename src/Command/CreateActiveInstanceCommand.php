031d3f891c260f4fd690d098027b8ef95c71f019
EmmanuelRaynal <eraynal.pro@gmail.com>
2022-03-07 01:23:26 +0100

    Command ok - Without parameters interactivity

--- src/Command/CreateActiveInstanceCommand.php
+++ src/Command/CreateActiveInstanceCommand.php
@@ -2,18 +2,13 @@
 
 namespace App\Command;
 
+use App\Entity\Checkpoint;
 use App\Entity\Game;
 use App\Entity\Instance;
+use App\Entity\Round;
+use App\Entity\ScanQR;
 use App\Entity\User;
-use App\Repository\AnswerRepository;
-use App\Repository\CheckpointRepository;
-use App\Repository\EnigmaRepository;
-use App\Repository\GameRepository;
-use App\Repository\InstanceRepository;
 use App\Repository\RoleRepository;
-use App\Repository\RoundRepository;
-use App\Repository\ScanQRRepository;
-use App\Repository\UserRepository;
 use DateInterval;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
@@ -59,9 +54,6 @@ class CreateActiveInstanceCommand extends Command
 
         /********** USERS **********/
 
-        // Users array for future use
-        $userObjects = [];
-
         // Get necessary Role objects 
         $organisatorRole = $this->roleRepository->findOneBy(['slug' => 'ROLE_ORGANISATEUR']);
         $playerRole = $this->roleRepository->findOneBy(['slug' => 'ROLE_JOUEUR']);
@@ -77,12 +69,14 @@ class CreateActiveInstanceCommand extends Command
         $organisator->setFirstname('Orga');
         $organisator->setStatus(true);
 
-        $userObjects[] = $organisator;
-
         $this->entityManager->persist($organisator);
 
         // Ten new players
-        for ($i = 1; $i<= 10; $i++) {
+
+        // Players array for next uses
+        $playerObjectsArray = [];
+
+        for ($i = 1; $i <= 10; $i++) {
             $player = new User();
             $player->setEmail("joueur$i@joueur.com");
             $player->setRole($playerRole);
@@ -92,7 +86,7 @@ class CreateActiveInstanceCommand extends Command
             $player->setFirstname('Joueur');
             $player->setStatus(true);
 
-            $userObjects[] = $player;
+            $playerObjectsArray[] = $player;
 
             $this->entityManager->persist($player);
         }
@@ -108,6 +102,23 @@ class CreateActiveInstanceCommand extends Command
 
         $this->entityManager->persist($game);
 
+        /********** CHECKPOINTS **********/
+
+        // Checkpoints array for next uses
+        $checkpointObjectsArray = [];
+
+        for ($i = 1; $i <= 5; $i++) {
+            $checkpoint = new Checkpoint();
+            $checkpoint->setTitle("Checkpoint de test créé par une commande ($i/5)");
+            $checkpoint->setOrderCheckpoint($i);
+            $checkpoint->setSuccessMessage("Bravo ! Rendez-vous maintenant au checkpoint suivant.");
+            $checkpoint->setGame($game);
+
+            $checkpointObjectsArray[] = $checkpoint;
+
+            $this->entityManager->persist($checkpoint);
+        }
+
         /********** INSTANCE **********/
 
         $instance = new Instance();
@@ -115,15 +126,69 @@ class CreateActiveInstanceCommand extends Command
         // get actual date
         $now = new DateTimeImmutable();
         // substract 1h to set the start hour of the instance
-        $start = $now->sub(new DateInterval('PT1H'));
-        $instance->setStartAt($start);
+        $InstanceStart = $now->sub(new DateInterval('PT1H'));
+        $instance->setStartAt($InstanceStart);
         // add 1h to set the end hour of the instance
-        $end = $now->add(new DateInterval('PT1H'));
-        $instance->setEndAt($end);
+        $InstanceEnd = $now->add(new DateInterval('PT1H'));
+        $instance->setEndAt($InstanceEnd);
         $instance->setGame($game);
         
         $this->entityManager->persist($instance);
-        
+
+        /********** ROUND **********/
+
+        // Rounds array for next uses
+        $roundObjectsArray = [];
+
+        // 1 round for each
+        foreach ($playerObjectsArray as $currentPlayer) {
+            $round = new Round();
+            // Round startAt = Instance startAt
+            $round->setStartAt($InstanceStart);
+            // No endAt, it will be set by the last checkpoint scan...
+            $round->setInstance($instance);
+            $round->setUser($currentPlayer);
+
+            $roundObjectsArray[] = $round;
+
+            $this->entityManager->persist($round);
+        }
+
+        /********** SCAN QR **********/
+
+        // Number of checkpoints of this Game
+        $numberOfCheckpoints = count($checkpointObjectsArray);
+
+        // For each round we generate many scans
+        foreach ($roundObjectsArray as $currentRound) {
+
+            // Random number of QR code scans (= how many checkpoints have been scanned ?)
+            $randomQRScanNumber = rand(1, $numberOfCheckpoints); 
+            // Var to calculate the next checkpoint scan datetime
+            $previousScanDate = $InstanceStart;
+
+            for ($i = 1; $i <= $randomQRScanNumber; $i++) {
+                $scanQR = new ScanQR();
+                // If first checkpoint : scan is the Round starAt (=instance startAt)
+                if ($i == 1) {
+                    $scanQR->setScanAt($InstanceStart);
+                // Else we add 10 min between each scan
+                } else {
+                    $scanAt = $previousScanDate->add(new DateInterval('PT10M'));
+                    $scanQR->setScanAt($scanAt);
+                    // Increment for next loop
+                    $previousScanDate = $scanAt;
+                }
+                // If last checkpoint : scan is the Round endAt
+                if ($i == $numberOfCheckpoints) {
+                    $currentRound->setEndAt($scanAt);
+                }
+                $scanQR->setCheckpoint($checkpointObjectsArray[$i-1]);
+                $scanQR->setRound($currentRound);
+
+                $this->entityManager->persist($scanQR);
+            }
+        }
 
         $this->entityManager->flush();
         
