57dfdb0799d09dbc4335ed26f28b09fc30c6b4ea
Damien-Oliveira <oliveiradamien123@gmail.com>
2022-03-08 10:08:38 +0100

    add score method in instanceController+twig

--- src/Controller/Front/InstanceController.php
+++ src/Controller/Front/InstanceController.php
@@ -31,6 +31,26 @@ class InstanceController extends AbstractController
         ]);
     }
 
+     /**
+     * @Route("/jeu/{gameSlug}/instance/{instanceSlug}/scores", name="app_front_instance_score", methods={"GET"})
+     */
+    public function score($gameSlug, $instanceSlug, GameRepository $gameRepository, InstanceRepository $instanceRepository): Response
+    {
+        // Get Game from slug
+        $game = $gameRepository->findOneBy(['slug' => $gameSlug]);
+
+        // Get Instance from slug
+        $instance = $instanceRepository->findOneBy(['slug' => $instanceSlug]);
+
+
+        return $this->render('front/instance/score.html.twig', [
+            'instance' => $instance,
+            'game' => $game,
+        ]);
+    }
+
+
+
     /**
      * @Route("/jeu/{gameSlug}/instance/{instanceSlug}/realtime", name="app_front_instance_realtime", methods={"GET"})
      */
