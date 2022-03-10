0361297380637cc111f9b9d6b9f7880d0cad948c
Sébastien <sgeraudie@gmail.com>
2022-03-10 09:09:20 +0100

    add token in login

--- src/Controller/Front/MainController.php
+++ src/Controller/Front/MainController.php
@@ -3,10 +3,13 @@
 namespace App\Controller\Front;
 
 use App\Repository\GameRepository;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class MainController extends AbstractController
 {    
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -11,7 +11,6 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
@@ -28,8 +27,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
     private UrlGeneratorInterface $urlGenerator;
     private UserRepository $userRepository;
     private SessionInterface $session;
-    private UserInterface $userInterface;
-    private JWTTokenManagerInterface $JWTManager;
+    private JWTTokenManagerInterface $JWTManager;    
 
     public function __construct(UrlGeneratorInterface $urlGenerator, UserRepository $userRepository, SessionInterface $session, JWTTokenManagerInterface $JWTManager)
     {
@@ -71,8 +69,9 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        //$this->session->set('token', $this->JWTManager->create($token->getUser()));
-        
+        $jwttoken = $this->JWTManager->create($token->getUser());
+        $this->session->set('token', $jwttoken);
+
         if($this->session->get('route_redirect') !== null)
         {
             return new RedirectResponse($this->session->get('route_redirect'));
