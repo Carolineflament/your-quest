8a2f8b44f45021a8231b7b71ff627b2006d595d9
EmmanuelRaynal <eraynal.pro@gmail.com>
2022-03-16 17:19:04 +0100

    Co-authored-by: Damien Oliveira <Damien-Oliveira@users.noreply.github.com> Co-authored-by: Caroline FLAMENT <Carolineflament@users.noreply.github.com> Co-authored-by: SebOClock <SebOClock@users.noreply.github.com>

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -48,7 +48,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         {
             $request->getSession()->getFlashBag()->add(
                 'notice-danger',
-                'Votre utilisateur a été désactivé par un administrateur, vous pouvez conctacter un admin@yourquest.fr'
+                'Votre utilisateur a été désactivé par un administrateur, vous pouvez nous conctacter en suivant le lien dans le pied de page'
             );
             
             return new Passport(new UserBadge(''), new PasswordCredentials(''));
